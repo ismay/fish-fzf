0ebbfa108fc15f0489bfe0394f69347c748cea5d
ismay <7355199+ismay@users.noreply.github.com>
2022-04-03 13:13:16 +0200

    Remove null flag

--- functions/_fzf_history.fish
+++ functions/_fzf_history.fish
@@ -2,15 +2,12 @@ function _fzf_history -d "Show command history"
     # history merge incorporates history changes from other fish sessions
     builtin history merge
 
-    set history_command builtin history \
-        --null
-
     set fzf_command fzf \
         --height 40% \
         --reverse \
         --color=16
 
-    set result ($history_command | $fzf_command | string collect)
+    set result (builtin history | $fzf_command | string collect)
 
     if [ -n "$result" ]
         commandline --replace -- (string escape $result)
