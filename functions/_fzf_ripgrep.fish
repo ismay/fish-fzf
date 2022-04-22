b733e8dd467d02ca0531dbea8de8db2f639afeda
ismay <7355199+ismay@users.noreply.github.com>
2022-04-22 09:56:27 +0200

    Update rg settings

--- functions/_fzf_ripgrep.fish
+++ functions/_fzf_ripgrep.fish
@@ -1,15 +1,11 @@
 function _fzf_ripgrep
-    set shared_rg_params --smart-case --fixed-strings
-
-    set rg_command rg \
-        --count \
-        $shared_rg_params
+    set rg_command rg --count
     set fzf_command fzf \
         --color=16 \
         --bind "change:reload:$rg_command {q} || true" \
         --disabled \
         --delimiter : \
-        --preview="rg $shared_rg_params --line-number --color always --context 2 {q} {1}" \
+        --preview="rg --line-number --color=always --context=2 {q} {1}" \
         --layout=reverse
 
     set result ($rg_command '' | $fzf_command)
